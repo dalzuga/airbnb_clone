1f6918fc15976fd62b2f3870d24da933bb1d4419
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-14 22:57:22 +0000

    pushing init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,12 @@
 CREATE USER 'airbnb_user_dev'@'%' IDENTIFIED BY 'aMDD,@q1#7"T';
 CREATE USER 'airbnb_user_prod'@'localhost' IDENTIFIED BY '7w6xZ;EJ14f,';
-
+CREATE DATABASE airbnb_dev
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+CREATE DATABASE airbnb_prod
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+USE airbnb_dev;
+USE airbnb_prod;
+GRANT ALL PRIVILEGES ON airbnb_dev . * TO 'airbnb_user_dev'@'localhost';
+GRANT ALL PRIVILEGES ON airbnb_prod . * TO 'airbnb_user_prod'@'localhost';
